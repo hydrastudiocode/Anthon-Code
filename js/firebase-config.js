f9a290cbb812b9b347fb58dd3ea06a95c77c903b
Hydra Studio Code <cjblackink@gmail.com>
2026-04-14 02:55:40 -0600

    Nueva Documentacion

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 const firebaseConfig = {
-  apiKey: "---",
-  authDomain: "---",
-  projectId: "---",
-  storageBucket: "---",
-  messagingSenderId: "",
-  appId: "---",
-  measurementId: "---"
+  apiKey: "AIzaSyDdhdhc8lj2N9O6d6YMX5UgGvrCATcTFE0",
+  authDomain: "anthon-fd7a0.firebaseapp.com",
+  projectId: "anthon-fd7a0",
+  storageBucket: "anthon-fd7a0.firebasestorage.app",
+  messagingSenderId: "730851414350",
+  appId: "1:730851414350:web:b52188592bcaa6b7358e1f",
+  measurementId: "G-0Z983EDNRY"
 };
 
 const app = initializeApp(firebaseConfig);
